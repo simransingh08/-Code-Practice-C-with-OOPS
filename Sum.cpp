0aef625002b72b7f5053606515c9bc5d500fdd03
Simran Singh <107848987+simransingh08@users.noreply.github.com>
2023-06-08 10:48:15 +0530

    2. Adding operation
    Adding the two no's

---
+++ Sum.cpp
@@ -0,0 +1,17 @@
+//Sum of two no's
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int a,b,sum;
+	cout<<"Enter first number = "<<endl;
+	cin>>a;
+	cout<<"Enter second number = "<<endl;
+	cin>>b;
+	sum = a+b;
+	cout<<"Sum of two numbers are = "<<sum;
+	return 0;
+	
+}
